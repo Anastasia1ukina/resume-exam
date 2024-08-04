963477c0a188bd33253f7ff87f6d0651c6ce5ded
Anastasia <lukina02anastasiy@gmail.com>
2024-08-05 01:19:14 +0300

    feat: добавляет редактирование текстовых элементов и кнопку "Скачать"

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-import "../css/style.css";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.js";
-
-document.querySelector("#app").innerHTML = `
-
-`;
-
-setupCounter(document.querySelector("#counter"));
+import '../css/style.css';
+import './download-pdf';
+import './editor';
